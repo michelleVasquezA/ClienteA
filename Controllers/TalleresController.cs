ff9fc85c9e1bbbe6d7da86b7f3acddc847977644
Brandon Rodriguez <brandon162001@gmail.com>
2023-10-28 15:05:00 -0500

    Inicio ok

---
+++ Controllers/TalleresController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ClienteA.Controllers
+{
+
+    public class TalleresController : Controller
+    {
+        private readonly ILogger<TalleresController> _logger;
+
+        public TalleresController(ILogger<TalleresController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
